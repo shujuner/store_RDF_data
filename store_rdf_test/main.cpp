718e015e3900cd9d8e1e4e856a3269f52b1cec93
shujun wang <2093317940@qq.com>
2018-12-17 20:39:20 +0800

    submmit

--- store_rdf_test/main.cpp
+++ store_rdf_test/main.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
-#include<vector>
-#include<string>
-using namespace std;
-struct triples
-{
-    string sub;
-    string pre;
-    string obj;
-};
-int main()
-{
-    vector<triples> data;
-    string s,p,o;
-    cout<<"请输入三个数据，以空格分割:"<<endl;
-    while(cin>>s>>p>>o)
-    {
-        triples triple;
-        triple.sub=s;
-        triple.pre=p;
-        triple.obj=o;
-        data.push_back(triple);
-        cout<<"现在开始输出目前vector中所有的数据--------------------------------"<<endl;
-        for(auto iter : data)
-        {
-            cout<<iter.sub<<" "<<iter.pre<<" "<<iter.obj<<endl;
-        }
-        cout<<"输出完毕"<<endl;
-        cout<<"请输入三个数据，以空格分割:"<<endl;
-    }
-}
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//using namespace std;
+//struct triples
+//{
+//    string sub;
+//    string pre;
+//    string obj;
+//};
+//int main()
+//{
+//    vector<triples> data;
+//    string s,p,o;
+//    cout<<"请输入三个数据，以空格分割:"<<endl;
+//    while(cin>>s>>p>>o)
+//    {
+//        triples triple;
+//        triple.sub=s;
+//        triple.pre=p;
+//        triple.obj=o;
+//        data.push_back(triple);
+//        cout<<"现在开始输出目前vector中所有的数据--------------------------------"<<endl;
+//        for(auto iter : data)
+//        {
+//            cout<<iter.sub<<" "<<iter.pre<<" "<<iter.obj<<endl;
+//        }
+//        cout<<"输出完毕"<<endl;
+//        cout<<"请输入三个数据，以空格分割:"<<endl;
+//    }
+//}
--- store_rdf_test/us_level_test_function.cpp
+++ store_rdf_test/us_level_test_function.cpp
@@ -1,19 +1,25 @@
-//#include<iostream>
-//#include <windows.h>
-//int main()
-//{
-//	double run_time;
-//	_LARGE_INTEGER time_start;
-//	_LARGE_INTEGER time_over;
-//	double dqFreq;
-//	LARGE_INTEGER f;
-//	QueryPerformanceFrequency(&f);
-//	dqFreq=(double)f.QuadPart;
-//	QueryPerformanceCounter(&time_start);
-//
-//
-//
-//	QueryPerformanceCounter(&time_over);
-//	run_time=1000000*(time_over.QuadPart-time_start.QuadPart)/dqFreq;
-//	printf("\nrun_time£º%fus\n",run_time);
-//}
+#include<iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    double run_time;
+	_LARGE_INTEGER time_start;
+	_LARGE_INTEGER time_over;
+	double dqFreq;
+	LARGE_INTEGER f;
+	QueryPerformanceFrequency(&f);
+    dqFreq=(double)f.QuadPart;
+    QueryPerformanceCounter(&time_start);
+
+    //function start
+
+    cout<<"1"<<endl;
+    cout<<"1"<<endl;
+
+    //function end
+
+    QueryPerformanceCounter(&time_over);
+    run_time=1000000*(time_over.QuadPart-time_start.QuadPart)/dqFreq;
+    printf("\nrun_time %fus\n",run_time);
+}
